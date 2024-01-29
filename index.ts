38209e30711684c09be2a97cfca3d6a2246d32b8
Gabriel Alves dos Santos <gabrielalvesdossantos16@gmail.com>
2024-01-28 23:42:44 -0400

    HashPassword

--- index.ts
+++ index.ts
@@ -4,6 +4,7 @@ import cors from 'cors';
 import { google } from 'googleapis';
 import stream from 'stream';
 import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcrypt'
 
 const prisma = new PrismaClient();
 
@@ -16,36 +17,89 @@ const upload = multer({ storage: storage });
 
 app.use(express.json());
 
+app.get('/', function(req, res){
+  res.send('Banco de dados');
+
+})
+
+
+app.get("/post", async function (req, res) {
+  const mostrar = await prisma.post.findMany();
+  res.json(mostrar);
+});
+
 app.get("/user", async function (req, res) {
   const mostrar = await prisma.user.findMany();
   res.json(mostrar);
 });
 
-app.post("/user/login", async function (req, res) {
-    try {
-      const { email, password } = req.body;
-      const userExists = await prisma.user.findUnique({ where: { email: email } });
-  
-      if (!userExists) {
-        res.json(false);
-      } else if (userExists.password === password) {
-        res.json(userExists);
-      } else {
-        res.json(null);
-      }
-    } catch (err) {
-      console.error('Erro ao validar login', err);
-      res.status(500).json({ error: 'Erro ao validar login' });
+
+app.post("/getuser", async function (req, res) {
+  try {
+    const {id} = req.body
+    const user = await prisma.user.findUnique({ where: { id:  Number(id)} });
+    res.json(user)
+  }catch (err) {
+    console.error('Erro ao validar Usuario', err);  
     }
-  });
+});
+
+
+app.post("/user/login", async function (req, res) {
+  try {
+    const { email, password } = req.body;
+    const userExists = await prisma.user.findUnique({ where: { email: email } });
+    if (!userExists) {
+      return res.json({ status:404 });
+    }
+    const checkPassword = await bcrypt.compare(password, userExists!.password)
+     if (!checkPassword) {
+      return  res.json({ status:401 });
+    } else {
+      return res.json({ status: 200, data: userExists });
+    }
+  } catch (err) {
+    console.error('Erro ao validar login', err);
+    res.status(500).json({ status: 500, error: 'Erro ao validar login' });
+  }
+});
   
   app.post("/user", upload.single('img'), async function (req, res) {
     try {
       const { name, email, password } = req.body;
+      
+      const userExists = await prisma.user.findUnique({ where: { email: email } });
+
+      if(userExists){
+        return res.json(422)
+      }
+
+      const salt = await bcrypt.genSalt(13)
+      const passwordHash = await bcrypt.hash(password,salt)
       const imageBuffer = req.file!.buffer;
   
       const link = await uploadFile(name, imageBuffer);
-      const novoUsuario = await prisma.user.create({ data: { name, email, password, img: link } });
+      const newuser = await prisma.user.create({ data: { name, email, password: passwordHash, img: link } });
+  
+      res.json(newuser);
+
+    } catch (err) {
+      console.error('Error creating user', err);
+      res.status(500).json({ error: 'Error creating user' });
+    }
+  });
+
+
+
+
+   
+  app.post("/post", upload.single('img'), async function (req, res) {
+    try {
+      const { title, content, authorId } = req.body;
+      const imageBuffer = req.file!.buffer;
+  
+      const link = await uploadFile(title, imageBuffer);
+      const novoUsuario = await prisma.post.create({ data: { title, content, img: link , authorId: Number(authorId) } });
   
       res.json(novoUsuario);
     } catch (err) {
